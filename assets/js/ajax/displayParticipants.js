256636c61d3056e8210ac34b2e8b1a025e9a4e8f
Harish <arishh2@gmail.com>
2019-08-21 19:59:24 +0530

    event admin panel view

--- assets/js/ajax/displayParticipants.js
+++ assets/js/ajax/displayParticipants.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
             var d = JSON.parse(data).result;
             console.log(d);
             var temp = '';
-            for (var i = 0; i < d.length; i++) {
+            for (var i = 1; i < d.length; i++) {
                 temp = temp + '<tr><td>' + (i + 1) + '</td><td>' + d[i].inno_id + '</td><td>' + d[i].name + '</td><td>' + d[i].phone + '</td><td>' + d[i].email + '</td><td>' + d[i].college + '</td></tr>';
             }
             $("#dp").append(temp);
