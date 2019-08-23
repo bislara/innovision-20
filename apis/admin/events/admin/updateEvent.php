5b5aba7420115bb08855b79f9c214f5e48cc8c95
Harish <arishh2@gmail.com>
2019-08-23 16:05:42 +0530

    Total Folder Restructure

--- apis/admin/events/admin/updateEvent.php
+++ apis/admin/events/admin/updateEvent.php
@@ -16,9 +16,7 @@
         
         $title=mysqli_real_escape_string($conn,$_POST['title']);
         
-        /* NOTE: The below query doesnt have date1 and 
-        time1 attributes since it didnt match with db */
-        
+             
         $query = "UPDATE events SET title ='".mysqli_real_escape_string($conn,$_POST['title'])."', description ='".mysqli_real_escape_string($conn,$_POST['description'])."', rules ='".mysqli_real_escape_string($conn,$_POST['rules'])."', judging_criteria ='".mysqli_real_escape_string($conn,$_POST['judging_criteria'])."', date ='".mysqli_real_escape_string($conn,$_POST['date'])."', date1 ='".mysqli_real_escape_string($conn,$_POST['date1'])."', venue ='".mysqli_real_escape_string($conn,$_POST['venue'])."', time ='".mysqli_real_escape_string($conn,$_POST['time'])."', time1 ='".mysqli_real_escape_string($conn,$_POST['time1'])."', category ='".mysqli_real_escape_string($conn,$_POST['category'])."', coordinatorName1 ='".mysqli_real_escape_string($conn,$_POST['coordinatorName1'])."', coordinatorContact1 ='".mysqli_real_escape_string($conn,$_POST['coordinatorContact1'])."', coordinatorName2 ='".mysqli_real_escape_string($conn,$_POST['coordinatorName2'])."', coordinatorContact2 ='".mysqli_real_escape_string($conn,$_POST['coordinatorContact2'])."',loginId ='".mysqli_real_escape_string($conn,$_POST['loginId'])."',loginPassword ='".mysqli_real_escape_string($conn,$_POST['loginPassword'])."',max_limit ='".mysqli_real_escape_string($conn,$_POST['max_limit'])."' WHERE eid =".$eid;
         //$query = "UPDATE events SET title ='".mysqli_real_escape_string($conn,$_POST['title'])."', description ='".mysqli_real_escape_string($conn,$_POST['description'])."', rules ='".mysqli_real_escape_string($conn,$_POST['rules'])."', judging_criteria ='".mysqli_real_escape_string($conn,$_POST['judging_criteria'])."', date ='".mysqli_real_escape_string($conn,$_POST['date'])."', venue ='".mysqli_real_escape_string($conn,$_POST['venue'])."', time ='".mysqli_real_escape_string($conn,$_POST['time'])."', category ='".mysqli_real_escape_string($conn,$_POST['category'])."', coordinatorName1 ='".mysqli_real_escape_string($conn,$_POST['coordinatorName1'])."', coordinatorContact1 ='".mysqli_real_escape_string($conn,$_POST['coordinatorContact1'])."', coordinatorName2 ='".mysqli_real_escape_string($conn,$_POST['coordinatorName2'])."', coordinatorContact2 ='".mysqli_real_escape_string($conn,$_POST['coordinatorContact2'])."',loginId ='".mysqli_real_escape_string($conn,$_POST['loginId'])."',loginPassword ='".mysqli_real_escape_string($conn,$_POST['loginPassword'])."' WHERE eid =".$eid;
         
@@ -32,8 +30,10 @@
                         
                     $ext = pathinfo($_FILES["fileToUpload"]["name"], PATHINFO_EXTENSION);
                     $file_path_name = "../../assets/images/events/eid_".($eid).('.').$ext;    
+                    $query="UPDATE events SET image_path='".$file_path_name."' WHERE eid='".$eid."'";
                     
                     if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $file_path_name)) {
+
                          echo(json_encode(array('status' => 'success', 'result' => 'successful entry')));
                     }
                     else {
