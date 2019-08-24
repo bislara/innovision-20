a70bee7783a0c44d8c3ee9222d2dc27031720ed3
Harish <arishh2@gmail.com>
2019-08-24 15:30:17 +0530

    event coordinator certificate done

--- apis/admin/events/coordinator/certificate.php
+++ apis/admin/events/coordinator/certificate.php
@@ -1,7 +1,7 @@
 <?php
     include('../../../db.php');
     include('./verifyAdmin.php');
-    
+
     $winner = $_POST['winner'];
     $runner = $_POST['runner'];
 
@@ -83,19 +83,18 @@
 
                     if(mysqli_num_rows($result4) > 0){
                             $query6 = "SELECT * FROM certificate WHERE img_path = 'images/event_results/".$runner_arr[$x]."A".$e_id.".jpg'";
-                            $result6 = mysqli_query($conn,$query6);
-                            print_r($result6);
+                            $result6 = mysqli_query($conn,$query6);                            
                             $row6 = mysqli_fetch_all($result6);
                             if(sizeof($row6) <= 0){
                                 $query3 = "INSERT INTO certificate (inno_id,img_path) VALUES(".mysqli_real_escape_string($conn,$runner_arr[$x]).",'images/event_results/".mysqli_real_escape_string($conn,$runner_arr[$x])."A".mysqli_real_escape_string($conn,$e_id).".jpg')";
                                 if(mysqli_query($conn,$query3)){
-                                    $output = '../../images/event_results/'.$runner_arr[$x].'A'.$e_id.'.jpg';
-                                    $image = imagecreatefromjpeg('../../images/event_results/result.jpg');
+                                    $output = '../../../../assets/images/event_results/'.$runner_arr[$x].'A'.$e_id.'.jpg';
+                                    $image = imagecreatefromjpeg('../../../../assets/images/event_results/result.jpg');
                                     $font_color = imagecolorallocate($image, 0, 0, 0);
-                                    $text = imagettftext($image,15,0,340,280,$font_color,"../../fonts/Charmonman/Charmonman-Bold.ttf",strtoupper($row4['name']));
-                                    $text1 = imagettftext($image,13,0,460,360,$font_color,"../../fonts/Charmonman/Charmonman-Bold.ttf",$row5['title']);
-                                    $text2 = imagettftext($image,15,0,200,320,$font_color,"../../fonts/Charmonman/Charmonman-Bold.ttf",$row4['college']);
-                                    $text3 = imagettftext($image,15,0,200,360,$font_color,"../../fonts/Charmonman/Charmonman-Bold.ttf","Runner");
+                                    $text = imagettftext($image,15,0,340,280,$font_color,"../../../../assets/fonts/Charmonman/Charmonman-Bold.ttf",strtoupper($row4['name']));
+                                    $text1 = imagettftext($image,13,0,460,360,$font_color,"../../../../assets/fonts/Charmonman/Charmonman-Bold.ttf",$row5['title']);
+                                    $text2 = imagettftext($image,15,0,200,320,$font_color,"../../../../assets/fonts/Charmonman/Charmonman-Bold.ttf",$row4['college']);
+                                    $text3 = imagettftext($image,15,0,200,360,$font_color,"../../../../assets/fonts/Charmonman/Charmonman-Bold.ttf","Runner");
                                     imagejpeg($image,$output);
 
                                     $flag3 = 1;
@@ -131,13 +130,13 @@
                             $query3 = "INSERT INTO certificate (inno_id,img_path) VALUES(".mysqli_real_escape_string($conn,$winner_arr[$x]).",'images/event_results/".mysqli_real_escape_string($conn,$winner_arr[$x])."A".mysqli_real_escape_string($conn,$e_id).".jpg')";
 
                         if(mysqli_query($conn,$query3)){
-                            $output = '../../images/event_results/'.$winner_arr[$x].'A'.$e_id.'.jpg';
-                            $image = imagecreatefromjpeg('../../images/event_results/result.jpg');
+                            $output = '../../../../assets/images/event_results/'.$winner_arr[$x].'A'.$e_id.'.jpg';
+                            $image = imagecreatefromjpeg('../../../../assets/images/event_results/result.jpg');
                             $font_color = imagecolorallocate($image, 0, 0, 0);
-                            $text = imagettftext($image,15,0,340,280,$font_color,"../../fonts/Charmonman/Charmonman-Bold.ttf",strtoupper($row4['name']));
-                            $text1 = imagettftext($image,13,0,460,360,$font_color,"../../fonts/Charmonman/Charmonman-Bold.ttf",$row5['title']);
-                            $text2 = imagettftext($image,15,0,200,320,$font_color,"../../fonts/Charmonman/Charmonman-Bold.ttf",$row4['college']);
-                            $text3 = imagettftext($image,15,0,200,360,$font_color,"../../fonts/Charmonman/Charmonman-Bold.ttf","Winner");
+                            $text = imagettftext($image,15,0,340,280,$font_color,"../../../../assets/fonts/Charmonman/Charmonman-Bold.ttf",strtoupper($row4['name']));
+                            $text1 = imagettftext($image,13,0,460,360,$font_color,"../../../../assets/fonts/Charmonman/Charmonman-Bold.ttf",$row5['title']);
+                            $text2 = imagettftext($image,15,0,200,320,$font_color,"../../../../assets/fonts/Charmonman/Charmonman-Bold.ttf",$row4['college']);
+                            $text3 = imagettftext($image,15,0,200,360,$font_color,"../../../../assets/fonts/Charmonman/Charmonman-Bold.ttf","Winner");
                             imagejpeg($image,$output);
                            
                             $flag4 = 1;
