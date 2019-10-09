9b7eb07ab033a515e9491585d5b8cebac7e7fea7
Harish <arishh2@gmail.com>
2019-10-09 15:44:18 +0530

    attempting sync with server

--- apis/admin/events/coordinator/login.php
+++ apis/admin/events/coordinator/login.php
@@ -7,8 +7,10 @@
 
         if( isset($_POST['loginid']) && isset($_POST['password'])) {            
             $query = mysqli_query($conn, "SELECT * FROM events WHERE loginId='".mysqli_real_escape_string($conn, $_POST['loginid'])."' AND loginPassword='".mysqli_real_escape_string($conn, $_POST['password'])."'");
+        	
             if(mysqli_num_rows($query) == 0)
             {
+            echo mysqli_error($conn); return;
                 echo json_encode(array('status'=>'failure','message'=>'invalid credentials'));
             }
             else
