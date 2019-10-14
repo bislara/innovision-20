9862ae90c828be26890e3b578d8bcba2eb0a54d6
Harish <arishh2@gmail.com>
2019-10-14 21:58:36 +0530

    show all participants in participants admin

--- apis/admin/participant/displayParticipantsList.php
+++ apis/admin/participant/displayParticipantsList.php
@@ -2,12 +2,18 @@
 
 	include('../../db.php');
 	include('./verifyParticipantsAdmin.php');
-	
+
 	$id=$_GET['q'];
 
-		if (isset($status) && $status === "success") {
+	
 
-			$query = mysqli_query($conn, "SELECT * FROM users WHERE inno_id='".$id."'");
+		
+		if (isset($status) && $status === "success") {			
+			if($id=='0'){
+				$query = mysqli_query($conn, "SELECT * FROM users");	
+			}else{
+				$query = mysqli_query($conn, "SELECT * FROM users WHERE inno_id='".$id."'");
+			}			
 
 			$result = array();
 			while($res = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
