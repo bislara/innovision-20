70f2e2d353440fbf65f2b6bea984a40251e22adb
WebminCMS <webmininno@gmail.com>
2019-10-22 12:51:25 +0000

    requirement admin update

--- apis/admin/participant/displayParticipantsList.php
+++ apis/admin/participant/displayParticipantsList.php
@@ -9,18 +9,21 @@
 
 		
 		if (isset($status) && $status === "success") {			
+        	$query = mysqli_query($conn, "SELECT count(*) cnt FROM users where paid='1'");	
+        	$count= mysqli_fetch_array($query,MYSQLI_ASSOC);			
 			if($id=='0'){
 				$query = mysqli_query($conn, "SELECT * FROM users");	
 			}else{
 				$query = mysqli_query($conn, "SELECT * FROM users WHERE inno_id='".$id."'");
 			}			
+        	
 
 			$result = array();
 			while($res = mysqli_fetch_array($query, MYSQLI_ASSOC)) {
 	    		$result[] = $res;
 			}
 
-			echo(json_encode(array('status' => 'success', 'result' => $result)));
+			echo(json_encode(array('status' => 'success', 'result' => $result,'paid'=>$count['cnt'])));
 		}
 	
 
