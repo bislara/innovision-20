7b4f7f6447534ceb5e48fa3512567b4df1144e40
Harish <arishh2@gmail.com>
2019-10-30 16:59:30 +0530

    allot hostels in participant admin

--- assets/js/ajax/fetchingParticipantsList.js
+++ assets/js/ajax/fetchingParticipantsList.js
@@ -1,11 +1,43 @@
+
 function populate(data_arr) {
   var str;
   var temp = '';
   for (var i = 0; i < data_arr.length; i++) {
       str = "";
+      let selectedIndex={
+        "def":"",
+        "cvr":"",
+        "kms":"",
+        "ssb":"",
+        "mv":"",
+        "gdb":"",
+        "mss":"",
+        "dba":"",
+        "hb":"",
+        "sd":"",
+        "vs":""
+      }
+      if(data_arr[i].allotted_hostel)
+        selectedIndex[data_arr[i].allotted_hostel]="selected";
+      else
+        selectedIndex["def"]="selected";
+
+      let hostelDropdown='<select class="hostelName">'+
+        '<option value="" selected='+selectedIndex["def"]+' disabled="disabled">Hostel</option>'+
+        '<option '+selectedIndex["cvr"]+' value="cvr">CVR</option>'+
+        '<option '+selectedIndex["kms"]+' value="kms">KMS</option>'+
+        '<option '+selectedIndex["ssb"]+' value="ssb">SSB</option>'+
+        '<option '+selectedIndex["mv"]+' value="mv">MV</option>'+
+        '<option '+selectedIndex["gdb"]+' value="gdb">GDB</option>'+
+        '<option '+selectedIndex["mss"]+' value="mss">MSS</option>'+
+        '<option '+selectedIndex["dba"]+' value="dba">DBA</option>'+
+        '<option '+selectedIndex["hb"]+' value="hb">HB</option>'+
+        '<option '+selectedIndex["sd"]+' value="sd">SD</option>'+
+        '<option '+selectedIndex["vs"]+' value="vs">VS</option>'+
+      '<select>';
       if (data_arr[i].checked_in == 0) str = '<td class="approval_checkbox"> <input class="checkbox" type="checkbox"> </td>';
       else str = '<td class="approval_checkbox"> <input class="checkbox" type="checkbox" checked> </td>';
-      temp = temp + '<tr><td style="display:none" id="inno_id">' + data_arr[i].inno_id + '</td><td>' + (i + 1) + '</td ><td>' + data_arr[i].inno_id + '</td><td>' + data_arr[i].name + '</td><td>' + data_arr[i].gender + '</td><td>' + data_arr[i].phone + '</td><td>' + data_arr[i].email + '</td><td>' + data_arr[i].college + '</td><td>' + data_arr[i].paid + '</td>' + str + '</tr>';
+      temp = temp + '<tr><td style="display:none" id="inno_id">' + data_arr[i].inno_id + '</td><td>' + (i + 1) + '</td ><td>' + data_arr[i].inno_id + '</td><td>' + data_arr[i].name + '</td><td>' + data_arr[i].gender + '</td><td>' + data_arr[i].phone + '</td><td>' + data_arr[i].email + '</td><td>' + data_arr[i].college + '</td><td>' + data_arr[i].paid + '</td><td class="hostel">' + hostelDropdown + '</td>' + str + '</tr>';
   }
   $("tbody").html(temp);
 }
