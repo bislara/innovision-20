bfce56136f7849aecd2811322636fa9b2f9c156c
WebminCMS <webmininno@gmail.com>
2019-10-31 09:54:28 +0000

    Events Update

--- assets/js/ajax/event_individual.js
+++ assets/js/ajax/event_individual.js
@@ -3,13 +3,18 @@ var eid = url.split("?")[1].split("=")[1];
 var status;
 var dataArray;
 setMetaTags=(eventArr)=>{
-	$('head').prepend('<title>'+eventArr["title"]+'</title>');
-	$('head').prepend('<meta name="keywords" content="'+eventArr["title"]+'">');
-	$('head').prepend('<meta name="title" content="'+eventArr["title"]+',innovision,inno nitrkl">');
-	$('head').prepend('<meta name="description" content="'+eventArr["description"]+'">');					
-	$('head').prepend('<meta name="image" content="../'+eventArr["image_path"] +'">');
-	$('head').prepend('<meta property="og:image:width" content="806">');
+	
+
 	$('head').prepend('<meta property="og:image:height" content="280">');
+	$('head').prepend('<meta property="og:image:width" content="806">');
+	$('head').prepend('<meta property="og:image" content="https://innonitr.com/'+eventArr["image_path"]+'">');
+	$('head').prepend('<meta property="og:url" content="'+window.location.href+'">');					
+	$('head').prepend('<meta property="og:type" content="website">');					
+	$('head').prepend('<meta property="og:description" content="'+eventArr["description"]+'">');
+	$('head').prepend('<meta property="og:title" content="'+eventArr["title"]+',innovision,inno nitrkl">');		
+	$('head').prepend('<meta name="keywords" content="'+eventArr["title"]+'">');
+	$('head').prepend('<title>'+eventArr["title"]+'</title>');	
+	$('head').prepend('<meta charset="utf-8">');	
 }
 function registerEvent()
 {
